2adbbcd8894bdfe0631c555949d0519648792e88
jfvillablanca <jmfvillablanca@gmail.com>
2022-10-24 16:50:13 +0800

    Add key capture and log to console

--- scripts.js
+++ scripts.js
@@ -10,6 +10,17 @@ const divide = function(a,b) {
     return a/b;
 };
 
-const subtract = function(a,b) {
+const multiply = function(a,b) {
     return a*b;
 };
+
+const registerKey = function(e) {
+  // keyPress = document.querySelector(`.button`);
+  console.log(`${e.key}`);
+};
+
+
+// const buttons = Array.from(document.querySelectorAll('.button'));
+// buttons.forEach(button => button.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',registerKey);
+
