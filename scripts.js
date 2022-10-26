6ac5d85da9cff4a20d123460a53231a3111426a1
jfvillablanca <jmfvillablanca@gmail.com>
2022-10-26 16:14:17 +0800

    Add backspace functionality

--- scripts.js
+++ scripts.js
@@ -17,18 +17,39 @@ const multiply = function(a,b) {
 const updateViewport = function(userInput,resetState) {
   viewport = document.querySelector('.calc-viewport');
   let viewportBuffer = viewport.innerText;
+  if (userInput === "←") { 
+    if (viewportBuffer.length === 1) { 
+      viewport.innerText = "0";
+      return;
+    }
+    else {
+      viewport.innerText = viewportBuffer.substring(0,viewportBuffer.length-1); 
+      console.log(`innertext: ${viewport.innerText}, buffer: ${viewportBuffer} `); //DIAGNOSTICS
+      return;
+    }
+  }
+  // else { viewportBuffer = viewport.innerText; }
+
   if (viewportBuffer === "0") { viewportBuffer = ""; }
-  console.log(viewportBuffer); //DIAGNOSTICS
   if (resetState) { viewport.innerText = "0"; }
   else {
-    // viewport.innerText = "";
-    viewport.innerText = `${viewportBuffer}${userInput}`;
+    if (userInput === "±") { console.log("plusminus"); }
+    else if (userInput === "÷") { console.log("divide"); }
+    else if (userInput === "×") { console.log("multiply"); }
+    else if (userInput === "-") { console.log("subtract"); }
+    else if (userInput === "+") { console.log("add"); }
+    else if (viewportBuffer.length !== 15 &&
+              userInput !== "←") { 
+      viewport.innerText = `${viewportBuffer}${userInput}`; 
+    }
   }
+  console.log(viewportBuffer); //DIAGNOSTICS
+  // viewport.innerText = viewportBuffer;
 }
 
 const registerClick = function(e) {
   let resetState = true;
-  console.log(e.target.innerText); //DIAGNOSTICS
+  // console.log(e.target.innerText); //DIAGNOSTICS
   e.target.classList.add('clicked');
   if (e.target.innerText !== "C") { resetState = false;}
   updateViewport(e.target.innerText,resetState)
