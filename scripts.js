f486b64949d4d051ce3d628620e5d27f81fdc218
jfvillablanca <jmfvillablanca@gmail.com>
2022-10-26 08:51:00 +0800

    Add click event listener

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const add = function(a,b) {
+    return a+b;
+};
+
+const subtract = function(a,b) {
+    return a-b;
+};
+
+const divide = function(a,b) {
+    return a/b;
+};
+
+const multiply = function(a,b) {
+    return a*b;
+};
+
+const registerClick = function(e) {
+  console.log(e.target);
+  e.target.classList.add('clicked');
+}
+
+const removeTransition = function(e) {
+  e.target.classList.remove('clicked');
+}
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach(button => button.addEventListener('click',registerClick));
+buttons.forEach(button => button.addEventListener('transitionend',removeTransition));
